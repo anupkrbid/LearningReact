276607b83e3476bdd77b2a7fb0770483969aba87
Tier5 LLC <github@tier5.us>
2017-08-03 17:37:11 +0530

    Redux Basic

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,34 @@
-alert('It Works!');
+import { createStore } from "redux";
+
+const reducer = function ( state, action) {
+
+	if ( action.type === 'ADD' ) {
+
+		return (state + action.payload);
+
+	} else if ( action.type === 'SUB' ) {
+
+		return (state - action.payload);
+
+	}
+	return state;
+};
+
+const store = createStore ( reducer, 0);
+
+store.subscribe ( () => console.log( 'State Changed', store.getState() ) );
+
+store.dispatch ( {
+	type: "ADD",
+	payload: 10
+} );
+
+store.dispatch ( {
+	type: "ADD",
+	payload: 10
+} );
+
+store.dispatch ( {
+	type: "SUB",
+	payload: 5
+} );
